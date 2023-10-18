43a621f52d89282c737eab6da6ea25bc13dd4c48
Katsarxs <91690260+Katsarxs@users.noreply.github.com>
2023-10-18 15:31:16 +0300

    Fixes

---
+++ escapeSequences.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+
+    /*
+
+    Escape Sequences
+
+    */
+
+    char a[] = "\a"; // \a : Alert (Beep, Bell)
+
+    char b[] = "\b"; // \b : Backspace
+
+    char e[] = "\e"; // \e : Escape character
+
+    char f[] = "\f"; // \f : Formfeed Page Break
+
+    char n[] = "\n"; // \n : Newline
+
+    char r[] = "\r"; // \r : Carriage Return
+
+    char t[] = "\t"; // \t : Horizontal Tab
+
+    char v[] = "\v"; // \v : Vertical Tab
+
+    char backslash[] = "\\"; // \\ : Backslash
+
+    char apostrophe[] = "\'"; // \' : Apostrophe
+
+    char doubleQuotationMark[] = "\""; // \" : Double Quotation Mark
+
+    char questionMark[] = "\?"; // \? : Question Mark
+
+    
+}
